57f537e1964190510aaf360b975f262528cead62
vidhya <vidhyasrik1997@gmail.com>
2024-09-14 17:22:43 +0530

    User login commit2

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import Home
 
 urlpatterns = [
-        path('user/', Home.as_view(), name='home'),
+        path('home/', Home.as_view(), name='home'),
 ]
--- myproject/security/views.py
+++ myproject/security/views.py
@@ -8,6 +8,7 @@ from .serailizers import UserSerializer
 from .models import MyUser
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.decorators import api_view
 
 
 
@@ -17,12 +18,17 @@ class UserRegistrationView(generics.CreateAPIView):
     queryset = MyUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
-    
+from django.http import JsonResponse
 class UserLoginView(APIView):
+    # permission_classes = (AllowAny,)
+    permission_classes = []
+    authentication_classes = []
+
     def post(self, request):
         user = authenticate(username=request.data['username'], password=request.data['password'])
+        print("User", user)
         if user:
-            Token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': Token.key})
+            token, created = Token.objects.get_or_create(user=user)
+            return Response([{"token": Token.key}])
         else:
             return Response({'error': 'Invalid username or password'}, status=401)
